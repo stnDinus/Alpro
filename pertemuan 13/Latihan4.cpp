05afc34df505fcdcc0e13dad676ce879e18a3934
stnDinus <111202214433@mhs.dinus.ac.id>
2023-06-25 09:13:51 +0700

    feat(tugas): pertemuan 13, Latihan4

--- pertemuan 13/Latihan4.cpp
+++ pertemuan 13/Latihan4.cpp
@@ -3,19 +3,27 @@
 #include <cstring>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 struct Mahasiswa {
-  char nim[15];
+  // attributes
+  char nim[14];
   string nama, jurusan;
   int tahunLulus;
 
+  // constructor
   Mahasiswa (string nim, string nama, string jurusan, int tahunLulus) {
     strcpy(this->nim, nim.c_str());
     this->nama = nama;
     this->jurusan = jurusan;
     this->tahunLulus = tahunLulus;
+  }
+
+  // methods
+  void printSelf () {
+    cout << this->nim << "\t" << this->nama << "\t" << this->jurusan << "\t" << this->tahunLulus << endl;
   }
 };
 
@@ -27,6 +35,27 @@ int main () {
     Mahasiswa("A11.2000.0123", "Ali", "DKV", 2003),
     Mahasiswa("A11.1990.0123", "Siti", "Kesehatan", 1993)
   };
+
+  // dinamis
+  vector<Mahasiswa> himpunanDinamisMahasiswa = {
+    Mahasiswa("A11.2020.0123", "Andi", "Broadcast", 2023),
+    Mahasiswa("A11.2010.0123", "Budi", "Sistem Informasi", 2013),
+    Mahasiswa("A11.2000.0123", "Ali", "DKV", 2003),
+    Mahasiswa("A11.1990.0123", "Siti", "Kesehatan", 1993)
+  };
+
+  // output
+  cout << "HIMPUNAN STATIS MAHASISWA\n";
+  for (Mahasiswa mhs : himpunanMahasiswa) {
+    mhs.printSelf();
+  }
+
+  cout << "\n";
+
+  cout << "HIMPUNAN DINAMIS MAHASISWA\n";
+  for (Mahasiswa mhs : himpunanDinamisMahasiswa) {
+    mhs.printSelf();
+  }
 
   return 0;
 }
