95c989b7b7afe2927a052cb45298185940dd4d36
stnDinus <111202214433@mhs.dinus.ac.id>
2023-06-26 13:47:48 +0700

    feat(tugas): perteumuan 14, binary search

--- pertemuan 14/a.cpp
+++ pertemuan 14/a.cpp
@@ -16,14 +16,39 @@ int linearSearch (vector<T> vec, T lookFor) {
   return -1;
 }
 
+int binarySearch(vector<int> vec, int lookFor, int endIndex, int startIndex = 0) {
+  int sliceLength = endIndex - startIndex;
+  int midIndex = startIndex + (sliceLength / 2);
+
+  if (sliceLength == 1) {
+    if (vec[startIndex] == lookFor) {
+      return startIndex;
+    } else {
+      return -1;
+    }
+  }
+
+  if (vec[midIndex] <= lookFor) {
+    return binarySearch(vec, lookFor, endIndex, midIndex);
+  } else {
+    return binarySearch(vec, lookFor, midIndex, startIndex);
+  }
+}
+
+
 int main () {
   Timer timer;
 
   vector<int> iVec = {1, 2, 3};
 
+  // searching
   int lookFor = 4;
-  int foundIndex = linearSearch(iVec, lookFor);
-  cout << "Linear Search: \t" << foundIndex << endl;
+
+  int linearFoundIndex = linearSearch(iVec, lookFor);
+  cout << "Linear Search: \t" << linearFoundIndex << endl;
+
+  int binaryFoundIndex = binarySearch(iVec, lookFor, iVec.size());
+  cout << "Binary Search: \t" << binaryFoundIndex << endl;
 
   cout << endl << "Main " << endl;
   return 0;
