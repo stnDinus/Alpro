d779b731d44420c5ffbf56de1acbec6f150ee0ab
stnDinus <111202214433@mhs.dinus.ac.id>
2023-06-30 12:15:38 +0700

    feat(tugas): pertemuan 14, quickSort

--- pertemuan 14/a.cpp
+++ pertemuan 14/a.cpp
@@ -4,6 +4,29 @@
 
 using namespace std;
 
+void quickSort(vector<int> * vec, int start_index, int end_index) {
+  if (start_index < end_index) {
+    // menggunakan titik acuan (pivot) elemen terakhir
+    int pivot = (*vec)[end_index];
+
+    // perbaiki index pivot [[
+    int pivot_index = start_index - 1; // index pivot awal
+    for (int i = start_index; i < end_index; i++) {
+      if ((*vec)[i] < pivot) {
+        swap((*vec)[i], (*vec)[++pivot_index]);
+      }
+    }
+    swap((*vec)[++pivot_index], (*vec)[end_index]);
+    // ]]
+
+    // recurse fungsi quickSort
+    quickSort(vec, start_index,
+              pivot_index - 1); // dari elemen awal hingga sebelum pivot
+    quickSort(vec, pivot_index + 1,
+              end_index); // dari elemen setelah pivot hingga akhir
+  }
+}
+
 template <typename T> int linearSearch(vector<T> vec, T lookFor) {
   for (int i = 0; i < vec.size(); i++) {
     if (vec[i] == lookFor) {
